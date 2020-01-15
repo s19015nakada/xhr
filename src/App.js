b37ba58ed755e21e67fa2e5f283038b80c72a0b3
ryusei <s19015@std.it-college.ac.jp>
2020-01-15 16:38:18 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
-const Greeting = props => <h1>{props.type}</h1>
-
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = { value: '' }
+  }
+
+  doChange (e) {
+    const newValue = e.target.value
+    this.setState({ value: newValue })
+  }
+
+  doSubmit (e) {
+    window.alert('値を送信: ' + this.state.value)
+    e.preventDefault()
   }
 
   render () {
+    const doSubmit = e => this.doSubmit(e)
+    const doChange = e => this.doChange(e)
     return (
-      <div>
-        <Greeting type='good' />
-      </div>
+      <form onSubmit={doSubmit}>
+        <input type='text' value={this.state.value} onChange={doChange} />
+        <input type='submit' value='送信' />
+      </form>
     )
   }
-  componentDidMount () {}
+}
 
 export default App;
