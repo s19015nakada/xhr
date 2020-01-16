dfec4b42d7659a96025f4c99dc7586e245de7b90
ryusei <s19015@std.it-college.ac.jp>
2020-01-16 14:43:14 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React from 'react';
 
-const Greeting = props => <h1>{props.type}</h1>
-
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+      this.state ={
+          name: 'クジラ',
+          age: 22,
+          hobby: '読書'
+      }
   }
 
-  render () {
+     doChange (e) {
+    const userValue = e.target.value
+    const key = e.target.name
+    this.setState({[key]: userValue})
+     }
+
+    doSubmit (e) {
+    e.preventDefault()
+    const j = JSON.stringify(this.state)
+    window.alert(j)
+    }
+
+    render () {
+         const doSubmit = (e) => this.doSubmit(e)
+        const doChange = (e) => this.doChange(e)
     return (
-      <div>
-        <Greeting type='good' />
-      </div>
+    <form onSubmit={doSubmit}>
+        <div><label>
+          名前: <br />
+          <input name='name'
+            type='text'
+            value={this.state.name}
+            onChange={doChange} />
+        </label></div>
+        <div><label>
+          年齢: <br />
+          <input name='age'
+            type='number'
+            value={this.state.age}
+            onChange={doChange} />
+        </label></div>
+        <div><label>
+          趣味: <br />
+          <input name='hobby'
+            type='text'
+            value={this.state.hobby}
+            onChange={doChange} />
+        </label></div>
+        <input type='submit' value='送信' />
+    </form>
     )
   }
-  componentDidMount () {}
+}
 
 export default App;
